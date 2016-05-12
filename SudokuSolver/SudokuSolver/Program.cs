93f44abb6d42d88ce67098c8ff3aa3e7af7548bc
Ravaniss <senechal.max06@gmail.com>
2016-05-12 11:57:16 +0200

    Add read file functionnality

--- SudokuSolver/SudokuSolver/Program.cs
+++ SudokuSolver/SudokuSolver/Program.cs
@@ -8,6 +8,22 @@ namespace SudokuSolver
 {
     class Program
     {
+        static void ReadFile()
+        {
+            int counter = 0;
+            string line;
+         
+            System.IO.StreamReader file =
+               new System.IO.StreamReader("c:\\test.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                counter++;
+            }
+            file.Close();
+            Console.ReadLine();
+        }
         static void Main(string[] args)
         {
         }
